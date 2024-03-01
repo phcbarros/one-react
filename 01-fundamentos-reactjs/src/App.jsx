649a13163b0e5b22e7bedda2eaf1af3414332006
Paulo Barros <paulo.barros@lambda3.com.br>
2024-03-01 17:53:48 -0300

    Iniciando com React.js - 01 - Componentes e Propriedades

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import {Post} from './Post'
 
+import './global.css'
+
+export function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div>
+      <Post
+        author="Paulo Barros"
+        content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere mollitia exercitationem rem quo ipsa aperiam amet vitae quibusdam illum doloremque quaerat expedita obcaecati ex quas, adipisci quasi, doloribus iste vero."
+      />
+      <Post
+        author="João Pessoa"
+        content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere mollitia exercitationem rem quo ipsa aperiam amet vitae quibusdam illum doloremque quaerat expedita obcaecati ex quas, adipisci quasi, doloribus iste vero."
+      />
+    </div>
   )
 }
-
-export default App
