74ca245998ccd6d38b491aeb6d64d716d6ece4f8
Paulo Barros <paulo.barros@lambda3.com.br>
2024-03-04 14:13:11 -0300

    Iniciando com React.js - 02 - CSS Modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,12 @@
 import {Post} from './Post'
+import {Header} from './components/Header'
 
 import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Paulo Barros"
         content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere mollitia exercitationem rem quo ipsa aperiam amet vitae quibusdam illum doloremque quaerat expedita obcaecati ex quas, adipisci quasi, doloribus iste vero."
