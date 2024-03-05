9ad5ebe532904fd4e5f6528d321517dee90a9821
Paulo Barros <paulo.barros@lambda3.com.br>
2024-03-05 09:58:46 -0300

    Iniciando com React.js - 02 - Component: Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,8 @@
 import {Post} from './Post'
 import {Header} from './components/Header'
+import {Sidebar} from './components/Sidebar'
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -7,14 +10,19 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Paulo Barros"
-        content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere mollitia exercitationem rem quo ipsa aperiam amet vitae quibusdam illum doloremque quaerat expedita obcaecati ex quas, adipisci quasi, doloribus iste vero."
-      />
-      <Post
-        author="João Pessoa"
-        content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere mollitia exercitationem rem quo ipsa aperiam amet vitae quibusdam illum doloremque quaerat expedita obcaecati ex quas, adipisci quasi, doloribus iste vero."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Paulo Barros"
+            content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere mollitia exercitationem rem quo ipsa aperiam amet vitae quibusdam illum doloremque quaerat expedita obcaecati ex quas, adipisci quasi, doloribus iste vero."
+          />
+          <Post
+            author="João Pessoa"
+            content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere mollitia exercitationem rem quo ipsa aperiam amet vitae quibusdam illum doloremque quaerat expedita obcaecati ex quas, adipisci quasi, doloribus iste vero."
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?q=80&w=500&q=50&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        {/* <img src="https://github.com/phcbarros.png" /> */}
+        <strong>Paulo Barros</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  )
+}
