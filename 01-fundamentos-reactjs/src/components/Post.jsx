a9f8014291285cb693034cf38a35c4613228975b
Paulo Barros <paulo.barros@lambda3.com.br>
2024-03-11 08:54:38 -0300

    Iniciando com React.js - 02 - Estilizacoes do Post

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -27,10 +27,12 @@ export function Post() {
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
         <p>
-          <a href="#">👉 jane.design/doctorcare</a>
+          <a href="#">jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="#">#novoprojeto #nlw #rocketseat</a>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#nlw</a>
+          <a href="#">#rocketseat</a>
         </p>
       </div>
     </article>
--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -8,10 +8,15 @@ export function Sidebar() {
       <img
         className={styles.cover}
         src="https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?q=80&w=500&q=50&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="imagem de uma tela de computador com código"
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/phcbarros.png" />
+        <img
+          className={styles.avatar}
+          src="https://github.com/phcbarros.png"
+          alt="foto de Paulo Barros"
+        />
         <strong>Paulo Barros</strong>
         <span>Web Developer</span>
       </div>
