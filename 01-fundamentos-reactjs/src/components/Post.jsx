0c55aab737950a23b9824d99c2428cbbfff9f7a3
Paulo Barros <paulo.barros@lambda3.com.br>
2024-03-11 08:19:35 -0300

    Iniciando com React.js - 02 - Component: Post

---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/phcbarros.png"
+            alt="Paulo Barros"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Paulo Barros</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time dateTime="2022-05-11 08:13:00" title="11 de maio às 08:13h">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
