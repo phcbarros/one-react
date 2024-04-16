9a1edc252e4d465ba1406a2a370da21363088e64
Paulo Barros <paulo.barros@lambda3.com.br>
2024-04-16 13:47:31 -0300

    Criando SPAs com ReactJS - 01 - Cores e fonte

--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -16,11 +16,15 @@ const ButtonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  background-color: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
 
-  ${(props) => {
+  /* ${(props) => {
     return css`
       background-color: ${ButtonVariants[props.variant]};
     `
-  }}
+  }} */
 `
