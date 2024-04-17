fdb4ebeb4fc9ae48ce55b5981dacebbc8433ab29
Paulo Barros <paulo.barros@lambda3.com.br>
2024-04-17 16:20:34 -0300

    Criando SPAs com ReactJS - 02 - Aprimorando os inputs

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -19,10 +19,24 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Backstage" />
+            <option value="Kubernetes" />
+            <option value="Micro Frontend" />
+            <option value="C#" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            min={5}
+            max={60}
+            step={5}
+          />
 
           <span>minutos.</span>
         </FormContainer>
