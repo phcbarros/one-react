fbc0c0da232f0b331e7576ae88861c125e2dc141
Paulo Barros <paulo.barros@lambda3.com.br>
2024-04-18 10:18:35 -0300

    Criando SPAs com ReactJS - 03 - React Hook Form

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import {Play} from 'phosphor-react'
+import {type FieldValues, useForm} from 'react-hook-form'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -9,10 +11,22 @@ import {
   TaskInput,
 } from './styles'
 
+// controlled = formulários simples com poucos campos em uma interface simples (login), tem maior fluidez, monitora o valor digitado em tempo real, exemplo usar useState
+// uncontrolled = formulários complexos com muitos campos, tem menor fluidez, não monitora o valor digitado em tempo real, exemplo usar eventos para atualizar os valores
+
 export function Home() {
+  const {register, handleSubmit, watch} = useForm()
+
+  function handleCreateNewCycle(data: FieldValues) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -20,6 +34,7 @@ export function Home() {
             id="task"
             placeholder="Dê um nome para o seu projeto"
             list="task-suggestions"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -36,6 +51,7 @@ export function Home() {
             min={5}
             max={60}
             step={5}
+            {...register('minutesAmount', {valueAsNumber: true})}
           />
 
           <span>minutos.</span>
@@ -49,7 +65,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButtonBase type="submit">
+        <StartCountdownButtonBase type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Começar
         </StartCountdownButtonBase>
