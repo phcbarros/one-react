2e180a7e05ac152c9e1a78e6cc4dd1061e88734a
Paulo Barros <paulo.barros@lambda3.com.br>
2024-04-24 15:23:11 -0300

    Criando SPAs com ReactJS - 06 - Salvando estado no storage

--- 02-ignite-timer/src/contexts/CyclesContext.tsx
+++ 02-ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,4 +1,5 @@
-import {createContext, useReducer, useState} from 'react'
+import {differenceInSeconds} from 'date-fns'
+import {createContext, useEffect, useReducer, useState} from 'react'
 import {
   addNewCycleAction,
   interruptCurrentCycleAction,
@@ -7,7 +8,7 @@ import {
 import {cyclesReducer, type Cycle} from '../reducers/cycles/reducer'
 
 interface CreateCycleData {
-  // separação de camadas, o contexto não deve ficar acoplado a interface (zod e hookforms)
+  // separação de camadas, o contexto não deve ficar acoplado a interface (zod e hookForms)
   task: string
   minutesAmount: number
 }
@@ -32,14 +33,41 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: Readonly<CyclesContextProviderProps>) {
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0',
+      )
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+
+      return initialState
+    },
+  )
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   const {cycles, activeCycleId} = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
+  })
 
   function markCurrentCycleAsFinished() {
     dispatch(markCurrentCycleAsFinishedAction())
