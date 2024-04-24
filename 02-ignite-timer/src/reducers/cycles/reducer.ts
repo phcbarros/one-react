5dce166345fbd6a4e95ac0424c2295d8d81fdc1b
Paulo Barros <paulo.barros@lambda3.com.br>
2024-04-24 15:09:37 -0300

    Criando SPAs com ReactJS - 06 - Usando Immer

--- 02-ignite-timer/src/reducers/cycles/reducer.ts
+++ 02-ignite-timer/src/reducers/cycles/reducer.ts
@@ -1,3 +1,4 @@
+import {produce} from 'immer'
 import {ActionTypes} from './actions'
 
 export interface Cycle {
@@ -23,33 +24,65 @@ export function cyclesReducer(state: CyclesState, action: any) {
   console.log(action)
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
+      // return {
+      //   ...state,
+      //   cycles: [...state.cycles, action.payload.newCycle],
+      //   activeCycleId: action.payload.newCycle.id,
+      // }
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
+
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) =>
+      //     cycle.id === state.activeCycleId
+      //       ? {...cycle, interruptedDate: new Date()}
+      //       : cycle,
+      //   ),
+      //   activeCycleId: null,
+      // }
+
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      })
+
+      if (currentCycleIndex < 0) {
+        return state
       }
 
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) =>
-          cycle.id === state.activeCycleId
-            ? {...cycle, interruptedDate: new Date()}
-            : cycle,
-        ),
-        activeCycleId: null,
+      return produce(state, (draft) => {
+        draft.cycles[currentCycleIndex].interruptedDate = new Date()
+        draft.activeCycleId = null
+      })
+    }
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) =>
+      //     cycle.id === state.activeCycleId
+      //       ? {...cycle, finishedDate: new Date()}
+      //       : cycle,
+      //   ),
+      //   activeCycleId: null,
+      //}
+
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      })
+
+      if (currentCycleIndex < 0) {
+        return state
       }
 
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) =>
-          cycle.id === state.activeCycleId
-            ? {...cycle, finishedDate: new Date()}
-            : cycle,
-        ),
-        activeCycleId: null,
-      }
+      return produce(state, (draft) => {
+        draft.cycles[currentCycleIndex].finishedDate = new Date()
+        draft.activeCycleId = null
+      })
+    }
 
     default:
       return state
