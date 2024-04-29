7d2267072422fddfef79f1d4bb60af8bd0d13260
Paulo Barros <paulo.barros@lambda3.com.br>
2024-04-29 14:13:46 -0300

    Consumo de API e performance ReactJS - 01 - Tabela de transações

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,12 +1,39 @@
 import {Header} from '../../components/Header'
 import {Summary} from '../../components/Summary'
-import {TransactionsContainer} from './styles'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
-    <TransactionsContainer>
+    <>
       <Header />
       <Summary />
-    </TransactionsContainer>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 3000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hotdog</td>
+              <td>
+                <PriceHighLight variant="outcome">-R$ 50,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </>
   )
 }
