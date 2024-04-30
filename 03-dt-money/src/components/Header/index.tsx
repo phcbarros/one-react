e9cd235997e2a775c00de3d2d78d4b33b557234f
Paulo Barros <paulo.barros@lambda3.com.br>
2024-04-30 14:12:37 -0300

    Consumo de API e performance ReactJS - 01 - Criando um modal acessível

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import logoImage from '../../assets/logo-ignite.svg'
 import {HeaderContainer, HeaderContent, NewTransactionButton} from './styles'
 
@@ -7,9 +8,21 @@ export function Header() {
       <HeaderContent>
         <img src={logoImage} alt="" />
 
-        <NewTransactionButton type="button">
-          Nova transação
-        </NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton type="button">
+              Nova transação
+            </NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Description />
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
