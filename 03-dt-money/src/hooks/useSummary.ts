bbe8f4e0577edd13f05130fcb3b00415c577ad81
Paulo Barros <paulo.barros@lambda3.com.br>
2024-05-03 18:14:54 -0300

    Consumo de API e performance ReactJS - 02 - Aplicando o useMemo

--- 03-dt-money/src/hooks/useSummary.ts
+++ 03-dt-money/src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import {useMemo} from 'react'
 import {useContextSelector} from 'use-context-selector'
 import {TransactionsContext} from '../contexts/TransactionsContext'
 
@@ -9,20 +10,22 @@ export function useSummary() {
   /**
    * Nenhum outro lugar a aplicação precisa da informação do total de entradas, saídas e  total
    * */
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {income: 0, outcome: 0, total: 0},
-  )
+        return acc
+      },
+      {income: 0, outcome: 0, total: 0},
+    )
+  }, [transactions])
 
   return summary
 }
