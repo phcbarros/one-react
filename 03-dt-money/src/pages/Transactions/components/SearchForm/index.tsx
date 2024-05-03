4fe335f21e6f748c0386ec70b05c4f6111562089
Paulo Barros <paulo.barros@lambda3.com.br>
2024-05-03 18:10:59 -0300

    Consumo de API e performance ReactJS - 02 - Aplicando o Memo

--- 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -6,6 +6,23 @@ import * as z from 'zod'
 import {TransactionsContext} from '../../../../contexts/TransactionsContext'
 import {SearchFormContainer} from './styles'
 
+/**
+ * Por que um componente renderiza?
+ * - Hooks changed (mudou estado, contexto, reducer)
+ * - Props changed (mudou propriedades)
+ * - Parent rerendered (componente pai renderizou)
+ *
+ * Qual o fluxo de renderização?
+ * 1. O React recria o HTML da interface daquele componente
+ * 2. Compara a versão do HTML recriada com a versão anterior
+ * 3. SE mudou alguma coisa, ele reescreve o HTML na tela
+ *
+ * Memo
+ * 0. Hooks changed, Props changed (deep comparison)
+ * 0.1 Comparar a versão anterior dos hooks e props
+ * 0.2 Se algo mudou, ele vai permitir que o React reescreva o HTML na tela
+ */
+
 const searchFormSchema = z.object({
   query: z.string(),
 })
@@ -48,3 +65,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+//export const SearchForm = memo(SearchFormComponent)
