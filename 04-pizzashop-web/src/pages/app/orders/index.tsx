4bb057af428b24cae9cd91f372243de0d37172b8
Paulo Barros <paulo.barros@lambda3.com.br>
2024-05-15 12:19:25 -0300

    Pizza Shop - 02 - Componente: Filtros de pedidos

--- 04-pizzashop-web/src/pages/app/orders/index.tsx
+++ 04-pizzashop-web/src/pages/app/orders/index.tsx
@@ -1,16 +1,15 @@
-import {ArrowRight, Search, X} from 'lucide-react'
 import {Helmet} from 'react-helmet-async'
 
-import {Button} from '@/components/ui/button'
-import {Input} from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import {OrderTableRow} from '@/pages/app/orders/order-table-row'
+
+import {OrderTableFilters} from './order-table-filters'
 
 export function Orders() {
   return (
@@ -21,10 +20,7 @@ export function Orders() {
       </div>
 
       <div className="space-y-2.5 pt-4">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <OrderTableFilters />
 
         <div className="border rounded-md">
           <Table>
@@ -36,50 +32,13 @@ export function Orders() {
                 <TableHead className="w-[140px]">Status</TableHead>
                 <TableHead>Cliente</TableHead>
                 <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
+                <TableHead className="w-[164px]" />
+                <TableHead className="w-[132px]" />
               </TableRow>
             </TableHeader>
             <TableBody>
               {Array.from({length: 10}).map((_, i) => (
-                <TableRow key={i}>
-                  <TableCell>
-                    <Button variant="outline" size="xs">
-                      <Search className="h-3 w-3" />
-                      <span className="sr-only">Detalhes do pedido</span>
-                    </Button>
-                  </TableCell>
-                  <TableCell className="font-mono text-sm font-medium">
-                    89uy9-dafadf9
-                  </TableCell>
-                  <TableCell className="text-muted-foreground">
-                    há 15 minutos
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex items-center gap-2">
-                      <span className="h-2 w-2 rounded-full bg-slate-400" />
-                      <span className="font-medium text-muted-foreground">
-                        Pendente
-                      </span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="font-medium">
-                    Paulo Henrique da Costa Barros
-                  </TableCell>
-                  <TableCell className="font-medium">R$ 150,00</TableCell>
-                  <TableCell>
-                    <Button variant="outline" size="xs">
-                      <ArrowRight className="mr-2 w-3 h-3" />
-                      Aprovar
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button variant="ghost" size="xs">
-                      <X className="mr-2 w-3 h-3" />
-                      Cancelar
-                    </Button>
-                  </TableCell>
-                </TableRow>
+                <OrderTableRow key={i} />
               ))}
             </TableBody>
           </Table>
