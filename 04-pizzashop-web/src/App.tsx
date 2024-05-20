e8a2c67663882e33a720c171020d5d3f60c4e7f5
Paulo Barros <paulo.barros@lambda3.com.br>
2024-05-20 17:55:04 -0300

    Pizza Shop - 03 - Configurando API client

--- 04-pizzashop-web/src/App.tsx
+++ 04-pizzashop-web/src/App.tsx
@@ -1,5 +1,6 @@
 import './global.css'
 
+import {QueryClientProvider} from '@tanstack/react-query'
 import {Helmet, HelmetProvider} from 'react-helmet-async'
 import {RouterProvider} from 'react-router-dom'
 import {Toaster} from 'sonner'
@@ -7,13 +8,17 @@ import {Toaster} from 'sonner'
 import {ThemeProvider} from '@/components/theme/theme-provider'
 import {router} from '@/routes'
 
+import {queryClient} from './lib/react-query'
+
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
