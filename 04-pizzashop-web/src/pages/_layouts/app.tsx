21c7c6b32becffc08c607b6cb3e2857b3548b041
Paulo Barros <paulo.barros@lambda3.com.br>
2024-05-27 17:25:56 -0300

    Pizza Shop - 03 - Métricas Dashboard

--- 04-pizzashop-web/src/pages/_layouts/app.tsx
+++ 04-pizzashop-web/src/pages/_layouts/app.tsx
@@ -1,8 +1,31 @@
-import {Outlet} from 'react-router-dom'
+import {isAxiosError} from 'axios'
+import {useEffect} from 'react'
+import {Outlet, useNavigate} from 'react-router-dom'
 
 import {Header} from '@/components/header'
+import {api} from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', {replace: true})
+          }
+        }
+      },
+    )
+
+    return () => api.interceptors.response.eject(interceptorId) // removendo o listener
+  }, [navigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
