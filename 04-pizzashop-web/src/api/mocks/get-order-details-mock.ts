d3ec5cfd1c9bac7ed98e744c56c097c686dc93a3
Paulo Barros <paulo.barros@lambda3.com.br>
2024-06-04 08:43:51 -0300

    Pizza Shop - 05 - Criando mocks dos pedidos

---
+++ 04-pizzashop-web/src/api/mocks/get-order-details-mock.ts
@@ -0,0 +1,42 @@
+import {http, HttpResponse} from 'msw'
+
+import type {
+  GetOrderDetailsParams,
+  GetOrderDetailsResponse,
+} from '../get-order-details'
+
+export const getOrderDetailsMock = http.get<
+  GetOrderDetailsParams,
+  never,
+  GetOrderDetailsResponse
+>('/orders/:orderId', async ({params}) => {
+  return HttpResponse.json({
+    id: params.orderId,
+    customer: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      phone: '123456789',
+    },
+    totalInCents: 3000,
+    status: 'pending',
+    createdAt: new Date().toISOString(),
+    orderItems: [
+      {
+        id: 'order-item-1',
+        priceInCents: 1000,
+        quantity: 1,
+        product: {
+          name: 'Pizza Marguerita',
+        },
+      },
+      {
+        id: 'order-item-2',
+        priceInCents: 2000,
+        quantity: 2,
+        product: {
+          name: 'Pizza Pepperoni',
+        },
+      },
+    ],
+  })
+})
