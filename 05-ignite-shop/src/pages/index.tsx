f35ccd1ceae657a8885b8691ab4f735156574925
Paulo Barros <paulo.barros@lambda3.com.br>
2024-07-23 17:44:49 -0300

    Ignite Shop - 04 - Trabalhando com SEO

--- 05-ignite-shop/src/pages/index.tsx
+++ 05-ignite-shop/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import {HomeContainer, Product} from '@/styles/pages/home'
 import 'keen-slider/keen-slider.min.css'
 import {useKeenSlider} from 'keen-slider/react'
+import Head from 'next/head'
 import Image from 'next/image'
 import {stripe} from '../lib/stripe'
 
@@ -28,24 +29,29 @@ export default function Home({products}: Readonly<HomeProps>) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        return (
-          <Link
-            href={`/product/${product.id}`}
-            key={product.id}
-            prefetch={false}>
-            <Product className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        )
-      })}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+              prefetch={false}>
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
--- 05-ignite-shop/src/pages/product/[id].tsx
+++ 05-ignite-shop/src/pages/product/[id].tsx
@@ -6,6 +6,7 @@ import {
 } from '@/styles/pages/product'
 import axios from 'axios'
 import {GetStaticPaths, GetStaticProps} from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import {useState} from 'react'
 import Stripe from 'stripe'
@@ -50,22 +51,29 @@ export default function Product({product}: ProductProps) {
   }
 
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt="" />
-      </ImageContainer>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ImageContainer>
 
-      <ProductDetail>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+        <ProductDetail>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <p>{product.description}</p>
+          <p>{product.description}</p>
 
-        <button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>
-          Comprar agora
-        </button>
-      </ProductDetail>
-    </ProductContainer>
+          <button
+            onClick={handleBuyProduct}
+            disabled={isCreatingCheckoutSession}>
+            Comprar agora
+          </button>
+        </ProductDetail>
+      </ProductContainer>
+    </>
   )
 }
 
