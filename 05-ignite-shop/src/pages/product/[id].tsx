3bf19d23fba33116cca88f3aaf8ab0e0b1b7695c
Paulo Barros <paulo.barros@lambda3.com.br>
2024-07-23 14:08:10 -0300

    Ignite Shop - 04 - Estrutura da página de produto

--- 05-ignite-shop/src/pages/product/[id].tsx
+++ 05-ignite-shop/src/pages/product/[id].tsx
@@ -1,6 +1,29 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetail,
+} from '@/styles/pages/product'
 import {useRouter} from 'next/router'
 
 export default function Product() {
   const {query} = useRouter()
-  return <h1>Product {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+
+      <ProductDetail>
+        <h1>Camiseta X</h1>
+        <span>R$ 59,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel harum
+          dicta officia impedit repellat, voluptatem officiis quia voluptates
+          quam vitae, et itaque vero praesentium ipsam quos sint. Quis, qui
+          sequi.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetail>
+    </ProductContainer>
+  )
 }
