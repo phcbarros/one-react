143c0b4ec82a57ed0f44ec5b2443621594f0db2d
Paulo Barros <paulo.barros@lambda3.com.br>
2024-07-23 16:27:52 -0300

    Ignite Shop - 04 - Estrutura de sucesso

--- 05-ignite-shop/src/pages/success.tsx
+++ 05-ignite-shop/src/pages/success.tsx
@@ -1,3 +1,21 @@
+import {ImageContainer, SuccessContainer} from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+        {/* <Image src="/images/rocket.svg" alt="" width={120} height={120} /> */}
+      </ImageContainer>
+
+      <p>
+        Uhull <strong>Paulo Barros</strong> sua <strong>Camiseta</strong> já
+        está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
