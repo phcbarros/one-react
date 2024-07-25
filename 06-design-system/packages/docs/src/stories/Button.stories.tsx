5da8d219a7460a5c0941cb28d53bd3566b7db5ca
Paulo Barros <paulo.barros@lambda3.com.br>
2024-07-25 09:28:43 -0300

    Design System - 02 - Documentação de Tokens

--- 06-design-system/packages/docs/src/stories/Button.stories.tsx
+++ 06-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import type {Meta, StoryObj} from '@storybook/react'
 import {Button, ButtonProps} from '@phcbarros-ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Enviar',
