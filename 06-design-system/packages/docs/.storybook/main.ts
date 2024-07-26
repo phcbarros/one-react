d081a41ab97b8fe7f36bc8e69f9f64b5cfdfd230
Paulo Barros <paulo.barros@lambda3.com.br>
2024-07-26 17:28:59 -0300

    Design System - 04 - Addon de acessibilidade

--- 06-design-system/packages/docs/.storybook/main.ts
+++ 06-design-system/packages/docs/.storybook/main.ts
@@ -18,6 +18,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-mdx-gfm'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
