5be076f7edc0eee04e63a07c2c64d4b506030719
Paulo Barros <paulo.barros@lambda3.com.br>
2024-08-01 15:20:19 -0300

    Ignite Call - 03 - Página de conexão do calendário

---
+++ 07-ignite-call/src/pages/register/connect-calendar/styles.ts
@@ -0,0 +1,47 @@
+import {Box, Heading, styled, Text} from '@ignite-ui/react'
+
+export const Container = styled('main', {
+  maxWidth: 572,
+  margin: '$20 auto $4',
+  padding: '0 $4',
+})
+
+export const Header = styled('div', {
+  padding: '0 $6',
+
+  [`> ${Heading}`]: {
+    lineHeight: '$base',
+  },
+
+  [`> ${Text}`]: {
+    color: '$gray200',
+    marginBottom: '$6',
+  },
+})
+
+export const ConnectBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const ConnectItem = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  border: '1px solid $gray600',
+  padding: '$4 $6',
+  borderRadius: '$md',
+
+  marginBottom: '$4',
+
+  [`${Heading}`]: {
+    lineHeight: '$base',
+  },
+
+  [`${Text}`]: {
+    color: '$gray200',
+    marginBottom: '$2',
+  },
+})
