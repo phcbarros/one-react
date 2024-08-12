5e3658c1abada48de9ad04a8c6e13569d89fabcb
Paulo Barros <paulo.barros@lambda3.com.br>
2024-08-12 09:35:01 -0300

    Ignite Call - 06 - Página de agendamento

---
+++ 07-ignite-call/src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,60 @@
+import {prisma} from '@/lib/prisma'
+import {Avatar, Heading, Text} from '@ignite-ui/react'
+import {GetStaticPaths, GetStaticProps} from 'next'
+import {Container, UserHeader} from './styles'
+
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24 // 1 dia
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({user}: Readonly<ScheduleProps>) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} alt={user.name} />
+        <Heading size="md">{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking', // true,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: ONE_DAY_IN_SECONDS,
+  }
+}
