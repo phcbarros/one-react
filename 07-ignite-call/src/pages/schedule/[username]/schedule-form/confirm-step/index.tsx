54e579545c0fb712fde912052a184cd9f4058be0
Paulo Barros <paulo.barros@lambda3.com.br>
2024-08-13 11:46:21 -0300

    Ignite Call - 06 - Página de confirmação

--- 07-ignite-call/src/pages/schedule/[username]/schedule-form/confirm-step/index.tsx
+++ 07-ignite-call/src/pages/schedule/[username]/schedule-form/confirm-step/index.tsx
@@ -1,3 +1,42 @@
+import {Button, Text, TextArea, TextInput} from '@ignite-ui/react'
+import {CalendarBlank, Clock} from 'phosphor-react'
+import {ConfirmForm, FormActions, FormHeader} from './styles'
+
 export function ConfirmStep() {
-  return <div></div>
+  return (
+    <ConfirmForm as="form" onSubmit={() => {}}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          24 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          08:00
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar agendamento</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
