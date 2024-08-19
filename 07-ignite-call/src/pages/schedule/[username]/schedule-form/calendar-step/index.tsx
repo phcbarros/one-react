a08be4b78bd2b340a0d13f8ceab02ab7b8ec5daf
Paulo Barros <paulo.barros@lambda3.com.br>
2024-08-19 12:25:51 -0300

    Ignite Call - 06 - Tabela de agendamentos

--- 07-ignite-call/src/pages/schedule/[username]/schedule-form/calendar-step/index.tsx
+++ 07-ignite-call/src/pages/schedule/[username]/schedule-form/calendar-step/index.tsx
@@ -1,4 +1,5 @@
 import {Calendar} from '@/components/calendar'
+import dayjs from 'dayjs'
 import {useState} from 'react'
 import {
   Container,
@@ -12,6 +13,11 @@ export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
   const isDateSelected = !!selectedDate
 
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
+
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onSelectedDate={setSelectedDate} />
@@ -19,7 +25,7 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            Terça-feira, <span>20 de setembro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
